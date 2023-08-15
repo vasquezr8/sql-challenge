c0c776b4c992b90dc0d492740245a759dbefc614
vasquezr8 <vasquezr8@gmail.com>
2023-08-15 15:49:51 -0700

    Aligned indentation on Create statements

--- EmployeeSQL/data_engineering.sql
+++ EmployeeSQL/data_engineering.sql
@@ -3,15 +3,15 @@
 -- titles, employees, departments, salaries, dept_manager, dept_emp
 
 CREATE TABLE titles (
-  title_id VARCHAR PRIMARY KEY,
-  title VARCHAR
+  	title_id VARCHAR PRIMARY KEY,
+  	title VARCHAR
 );
 
 SELECT * FROM salaries;
 
 CREATE TABLE employees (
-  emp_no INTEGER PRIMARY KEY,
-  emp_title_id VARCHAR,
+  	emp_no INTEGER PRIMARY KEY,
+  	emp_title_id VARCHAR,
 	birth_date DATE,
 	first_name VARCHAR,
 	last_name VARCHAR,
@@ -21,28 +21,28 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE departments (
-  dept_no VARCHAR PRIMARY KEY,
-  dept_name VARCHAR
+  	dept_no VARCHAR PRIMARY KEY,
+  	dept_name VARCHAR
 );
 
 CREATE TABLE dept_manager (
-  dept_no VARCHAR,
-  emp_no INTEGER,
+  	dept_no VARCHAR,
+  	emp_no INTEGER,
 	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
 	PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE dept_emp (
-  emp_no INTEGER,
-  dept_no VARCHAR,
+  	emp_no INTEGER,
+  	dept_no VARCHAR,
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
-  emp_no INTEGER PRIMARY KEY,
-  salary INTEGER,
+  	emp_no INTEGER PRIMARY KEY,
+  	salary INTEGER,
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
